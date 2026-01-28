85a614e483adfbd7d15f7f9fab284248f4cc6d1f
kaynarkerem-ai <kaynarkerem@gmail.com>
2026-01-28 15:19:48 +0300

    df

--- inventory.js
+++ inventory.js
@@ -1,198 +1,219 @@
 const productsDB = [
     {
-        "code": "MandarinCollarSuits-1023D170",
         "mainCat": "Wedding Suits",
         "subCat": "Mandarin Collar Suits",
+        "code": "1023D170",
         "realCode": "1023D170",
-        "folderPath": "Wedding Suits/Mandarin Collar Suits/1023D170"
+        "folderPath": "Wedding Suits/1023D170"
     },
     {
-        "code": "MandarinCollarSuits-50055-6",
         "mainCat": "Wedding Suits",
         "subCat": "Mandarin Collar Suits",
+        "code": "50055-6",
         "realCode": "50055-6",
-        "folderPath": "Wedding Suits/Mandarin Collar Suits/50055-6"
+        "folderPath": "Wedding Suits/50055-6"
     },
     {
-        "code": "MandarinCollarSuits-50059-1",
         "mainCat": "Wedding Suits",
         "subCat": "Mandarin Collar Suits",
+        "code": "50059-1",
         "realCode": "50059-1",
-        "folderPath": "Wedding Suits/Mandarin Collar Suits/50059-1"
+        "folderPath": "Wedding Suits/50059-1"
     },
     {
-        "code": "MandarinCollarSuits-50111-2 laci",
         "mainCat": "Wedding Suits",
         "subCat": "Mandarin Collar Suits",
+        "code": "50111-2 laci",
         "realCode": "50111-2 laci",
-        "folderPath": "Wedding Suits/Mandarin Collar Suits/50111-2 laci"
+        "folderPath": "Wedding Suits/50111-2 laci"
     },
     {
-        "code": "MandarinCollarSuits-50111-3",
         "mainCat": "Wedding Suits",
         "subCat": "Mandarin Collar Suits",
+        "code": "50111-3",
         "realCode": "50111-3",
-        "folderPath": "Wedding Suits/Mandarin Collar Suits/50111-3"
+        "folderPath": "Wedding Suits/50111-3"
     },
     {
-        "code": "MandarinCollarSuits-50111-9",
         "mainCat": "Wedding Suits",
         "subCat": "Mandarin Collar Suits",
+        "code": "50111-9",
         "realCode": "50111-9",
-        "folderPath": "Wedding Suits/Mandarin Collar Suits/50111-9"
+        "folderPath": "Wedding Suits/50111-9"
     },
     {
-        "code": "MandarinCollarSuits-50200-14",
         "mainCat": "Wedding Suits",
         "subCat": "Mandarin Collar Suits",
+        "code": "50200-14",
         "realCode": "50200-14",
-        "folderPath": "Wedding Suits/Mandarin Collar Suits/50200-14"
+        "folderPath": "Wedding Suits/50200-14"
     },
     {
-        "code": "MandarinCollarSuits-50200-42",
         "mainCat": "Wedding Suits",
         "subCat": "Mandarin Collar Suits",
+        "code": "50200-42",
         "realCode": "50200-42",
-        "folderPath": "Wedding Suits/Mandarin Collar Suits/50200-42"
+        "folderPath": "Wedding Suits/50200-42"
     },
     {
-        "code": "MandarinCollarSuits-50201-1 koyu siyah",
         "mainCat": "Wedding Suits",
         "subCat": "Mandarin Collar Suits",
+        "code": "50201-1 koyu siyah",
         "realCode": "50201-1 koyu siyah",
-        "folderPath": "Wedding Suits/Mandarin Collar Suits/50201-1 koyu siyah"
+        "folderPath": "Wedding Suits/50201-1 koyu siyah"
     },
     {
-        "code": "MandarinCollarSuits-50201-24",
         "mainCat": "Wedding Suits",
         "subCat": "Mandarin Collar Suits",
+        "code": "50201-24",
         "realCode": "50201-24",
-        "folderPath": "Wedding Suits/Mandarin Collar Suits/50201-24"
+        "folderPath": "Wedding Suits/50201-24"
     },
     {
-        "code": "ShawlCollarSmokings-50112-22",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50003-01",
+        "realCode": "50003-01",
+        "folderPath": "Wedding Suits/50003-01"
+    },
+    {
+        "mainCat": "Wedding Suits",
+        "subCat": "Shawl Collar Smokings",
+        "code": "50005-01",
+        "realCode": "50005-01",
+        "folderPath": "Wedding Suits/50005-01"
+    },
+    {
+        "mainCat": "Wedding Suits",
+        "subCat": "Shawl Collar Smokings",
+        "code": "50005-02",
+        "realCode": "50005-02",
+        "folderPath": "Wedding Suits/50005-02"
+    },
+    {
+        "mainCat": "Wedding Suits",
+        "subCat": "Shawl Collar Smokings",
+        "code": "50112-22",
         "realCode": "50112-22",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50112-22"
+        "folderPath": "Wedding Suits/50112-22"
     },
     {
-        "code": "ShawlCollarSmokings-50113-22",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50113-22",
         "realCode": "50113-22",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50113-22"
+        "folderPath": "Wedding Suits/50113-22"
     },
     {
-        "code": "ShawlCollarSmokings-50113-6",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50113-6",
         "realCode": "50113-6",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50113-6"
+        "folderPath": "Wedding Suits/50113-6"
     },
     {
-        "code": "ShawlCollarSmokings-50114-3",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50114-3",
         "realCode": "50114-3",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50114-3"
+        "folderPath": "Wedding Suits/50114-3"
     },
     {
-        "code": "ShawlCollarSmokings-50114-6",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50114-6",
         "realCode": "50114-6",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50114-6"
+        "folderPath": "Wedding Suits/50114-6"
     },
     {
-        "code": "ShawlCollarSmokings-50116-01",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50116-01",
         "realCode": "50116-01",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50116-01"
+        "folderPath": "Wedding Suits/50116-01"
     },
     {
-        "code": "ShawlCollarSmokings-50116-02",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50116-02",
         "realCode": "50116-02",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50116-02"
+        "folderPath": "Wedding Suits/50116-02"
     },
     {
-        "code": "ShawlCollarSmokings-50123-2",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50123-2",
         "realCode": "50123-2",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50123-2"
+        "folderPath": "Wedding Suits/50123-2"
     },
     {
-        "code": "ShawlCollarSmokings-50125-2",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50125-2",
         "realCode": "50125-2",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50125-2"
+        "folderPath": "Wedding Suits/50125-2"
     },
     {
-        "code": "ShawlCollarSmokings-50127-1",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50127-1",
         "realCode": "50127-1",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50127-1"
+        "folderPath": "Wedding Suits/50127-1"
     },
     {
-        "code": "ShawlCollarSmokings-50127-10",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50127-10",
         "realCode": "50127-10",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50127-10"
+        "folderPath": "Wedding Suits/50127-10"
     },
     {
-        "code": "ShawlCollarSmokings-50127-13",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50127-13",
         "realCode": "50127-13",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50127-13"
+        "folderPath": "Wedding Suits/50127-13"
     },
     {
-        "code": "ShawlCollarSmokings-50127-17",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50127-17",
         "realCode": "50127-17",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50127-17"
+        "folderPath": "Wedding Suits/50127-17"
     },
     {
-        "code": "ShawlCollarSmokings-50127-2",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50127-2",
         "realCode": "50127-2",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50127-2"
+        "folderPath": "Wedding Suits/50127-2"
     },
     {
-        "code": "ShawlCollarSmokings-50127-24",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50127-24",
         "realCode": "50127-24",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50127-24"
+        "folderPath": "Wedding Suits/50127-24"
     },
     {
-        "code": "ShawlCollarSmokings-50127-42",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50127-42",
         "realCode": "50127-42",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50127-42"
+        "folderPath": "Wedding Suits/50127-42"
     },
     {
-        "code": "ShawlCollarSmokings-50128-43",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "50128-43",
         "realCode": "50128-43",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/50128-43"
+        "folderPath": "Wedding Suits/50128-43"
     },
     {
-        "code": "ShawlCollarSmokings-AGM 4043",
         "mainCat": "Wedding Suits",
         "subCat": "Shawl Collar Smokings",
+        "code": "AGM 4043",
         "realCode": "AGM 4043",
-        "folderPath": "Wedding Suits/Shawl Collar Smokings/AGM 4043"
+        "folderPath": "Wedding Suits/AGM 4043"
     }
 ];
